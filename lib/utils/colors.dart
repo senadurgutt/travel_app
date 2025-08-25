53621bdd0cff943d1c9ca7d08a4fe71508566902
Sena Durgut <91386955+senadurgutt@users.noreply.github.com>
2025-08-25 14:51:00 +0300

    Appcolors eklendi

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color background = Color(0xFFFDF0D5);
+  static Color card = Color(0xFFFFFFFF);
+  static Color primaryText = Color(0xFF003049);
+  static Color secondaryText = Color(0xFF780000);
+  static Color favorite = Color(0xFFc1121f);
+}
