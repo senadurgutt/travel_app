2fc2527bceab42344961fda27e619a064d8d2fb3
Sena Durgut <91386955+senadurgutt@users.noreply.github.com>
2025-08-25 14:52:05 +0300

    sayfalara balık ve renkler eklendi

--- lib/views/main_page.dart
+++ lib/views/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:travel_app/controllers/navbar_controller.dart';
+import 'package:travel_app/utils/colors.dart';
 import 'package:travel_app/views/Favorites/favorites_page.dart';
 import 'package:travel_app/widgets/bottom_navbar_widget.dart';
 import 'home_page.dart';
@@ -16,14 +17,57 @@ class MainPage extends StatelessWidget {
     HomePage(),
     ProfilePage(),
   ];
+  final pageTitles = [
+    "Favoriler",
+    "Ana Sayfa",
+    "Profil",
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Obx(() => IndexedStack(
-            index: navController.currentIndex.value,
-            children: pages,
-          )),
+      backgroundColor: AppColors.background,
+      body: SafeArea(
+        child: Obx(() {
+          final currentIndex = navController.currentIndex.value;
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // BAŞLIK
+              Padding(
+                
+                padding: EdgeInsetsGeometry.only(top: 20.0 , left: 20),
+                child: Text(
+                  pageTitles[currentIndex],
+                  style: TextStyle(
+                    color: AppColors.primaryText,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+
+              // BORDER İÇİNDE SAYFALAR
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(12),
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: AppColors.card,
+                    border: Border.all(color: Colors.grey.shade300, width: 2),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: IndexedStack(
+                    index: currentIndex,
+                    children: pages,
+                  ),
+                ),
+              ),
+            ],
+          );
+        }),
+      ),
       bottomNavigationBar: BottomNavBar(),
     );
   }
